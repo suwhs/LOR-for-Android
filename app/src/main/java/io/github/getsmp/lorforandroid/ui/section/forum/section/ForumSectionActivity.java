534568f9621e39caf55367f51d197ff432244bc4
getsmp <getsmp@users.noreply.github.com>
2016-02-08 12:47:52 +0300

    ForumSectionActivity

---
+++ app/src/main/java/io/github/getsmp/lorforandroid/ui/section/forum/section/ForumSectionActivity.java
@@ -0,0 +1,26 @@
+package io.github.getsmp.lorforandroid.ui.section.forum.section;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.github.getsmp.lorforandroid.R;
+
+public class ForumSectionActivity extends AppCompatActivity {
+    @Bind(R.id.toolbarTop) Toolbar toolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String group = getIntent().getStringExtra("group");
+        ForumSectionFragment fragment = ForumSectionFragment.newInstance(group);
+        getSupportFragmentManager().beginTransaction().replace(R.id.forumSectionFragment, fragment).commit();
+    }
+}
